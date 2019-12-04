c91e14735db61ad1cf602fee08bde150916b5000
Hop Vu Van <hopvuvan91@hotmail.com>
2019-12-04 21:28:31 +0700

    add firebase

---
+++ src/firebase/myFirebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyCtctKoZYK3zZ5kbOXWBWxVrqTmrhWxi9s",
+  authDomain: "chat-simple-app.firebaseapp.com",
+  databaseURL: "https://chat-simple-app.firebaseio.com",
+  projectId: "chat-simple-app",
+  storageBucket: "chat-simple-app.appspot.com",
+  messagingSenderId: "64435657138",
+  appId: "1:64435657138:web:77dcc3c15499da10cf2cab",
+  measurementId: "G-E0N3B9S1H6"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({
+  timestampsInSnapshots: true
+});
+
+export const myFirebase = firebase;
+export const myFirestore = firebase.firestore();
+export const myStorage = firebase.storage();
